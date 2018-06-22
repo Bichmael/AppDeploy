e21e17849bfd5e72996ea25926727b5b5f1ce900
Bichmael <Bichmael@users.noreply.github.com>
2018-06-22 10:19:12 +0200

    Bloomberg Mai 2018

--- Deploy-Application.ps1
+++ Deploy-Application.ps1
@@ -61,19 +61,19 @@ Try {
 	##* VARIABLE DECLARATION
 	##*===============================================
 	## Variables: Application
-	[string]$appVendor = ''
-	[string]$appName = ''
-	[string]$appVersion = ''
-	[string]$appArch = ''
+	[string]$appVendor = 'Bloomberg'
+	[string]$appName = 'Bloomber Professional'
+	[string]$appVersion = 'Mai 2018'
+	[string]$appArch = 'x64'
 	[string]$appLang = 'EN'
 	[string]$appRevision = '01'
 	[string]$appScriptVersion = '1.0.0'
-	[string]$appScriptDate = '02/12/2017'
-	[string]$appScriptAuthor = '<author name>'
+	[string]$appScriptDate = '19/06/2018'
+	[string]$appScriptAuthor = 'Mickael Hofer'
 	##*===============================================
 	## Variables: Install Titles (Only set here to override defaults set by the toolkit)
-	[string]$installName = ''
-	[string]$installTitle = ''
+	[string]$installName = 'Bloomberg Update Mai 2018'
+	[string]$installTitle = 'Bloomberg Update Mai 2018'
 	
 	##* Do not modify section below
 	#region DoNotModify
@@ -116,8 +116,8 @@ Try {
 		##*===============================================
 		[string]$installPhase = 'Pre-Installation'
 
-		## Show Welcome Message, close Internet Explorer if required, allow up to 3 deferrals, verify there is enough disk space to complete the install, and persist the prompt
-		Show-InstallationWelcome -CloseApps 'iexplore' -AllowDefer -DeferTimes 3 -CheckDiskSpace -PersistPrompt
+		## Show Welcome Message, close Internet Bloomberg if required, allow up to 3 deferrals, verify there is enough disk space to complete the install, and persist the prompt
+		Show-InstallationWelcome -CloseApps 'wintrv' -ForceCountdown 10
 		
 		## Show Progress Message (with the default message)
 		Show-InstallationProgress
@@ -137,7 +137,7 @@ Try {
 		}
 		
 		## <Perform Installation tasks here>
-		
+		Execute-Process -Path "sotrt05072018.exe" -Parameters "/S"
 		
 		##*===============================================
 		##* POST-INSTALLATION
@@ -147,7 +147,7 @@ Try {
 		## <Perform Post-Installation tasks here>
 		
 		## Display a message at the end of the install
-		If (-not $useDefaultMsi) { Show-InstallationPrompt -Message 'You can customize text to appear at the end of an install or remove it completely for unattended installations.' -ButtonRightText 'OK' -Icon Information -NoWait }
+		If (-not $useDefaultMsi) { Show-InstallationPrompt -Message 'Bloomberg Update Mai 2018 is now installed.`nYou could close this windows' -ButtonRightText 'OK' -Icon Information -NoWait }
 	}
 	ElseIf ($deploymentType -ieq 'Uninstall')
 	{
@@ -177,7 +177,7 @@ Try {
 		}
 		
 		# <Perform Uninstallation tasks here>
-		
+		Execute-Process -Path "C:\ProgramData\{59FC24FF-8E39-4EA0-9406-9D3C5EB7FDCF}\sotrt.exe" -Parameters "REMOVE=TRUE /S"
 		
 		##*===============================================
 		##* POST-UNINSTALLATION
